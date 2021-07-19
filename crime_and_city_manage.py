375c99be635294d43babd57b7a342e75c5633743
cao <61373015+reporter-law@users.noreply.github.com>
2021-07-19 15:25:21 +0800

    update

--- crime_and_city_manage.py
+++ crime_and_city_manage.py
@@ -6,18 +6,33 @@
 # 收获:
 from tqdm import tqdm
 crimes,cities = [],[]
-with open(r"J:\PyCharm项目\项目\中国裁判文书网\selenium路线\法条内容提取\罪名与法定刑\刑法罪名.txt","r")as f:
+with open(r"J:\PyCharm项目\项目\中国裁判文书网\selenium路线\文书爬取模块\获取城市限定模块\常见刑法罪名.txt","r",encoding="utf-8")as f:
     for i in f.readlines():
         i = i.strip()
         crimes.append(i)
-with open(r"J:\PyCharm项目\项目\中国裁判文书网\selenium路线\数据清洗模块\城市（副本）.txt","r",encoding="utf-8")as f_:
+with open(r"J:\PyCharm项目\项目\中国裁判文书网\selenium路线\文书爬取模块\获取城市限定模块\city.txt","r",encoding="utf-8")as f_:
     for i in f_.readlines():
         i = i.strip()
         cities.append(i)
-for i in tqdm(crimes):
-    for i_ in cities:
-        with open(r"J:\PyCharm项目\项目\中国裁判文书网\selenium路线\文书爬取模块\crime_and_cities.txt", "a",encoding="utf-8")as f1:
-            f1.write(i+","+i_+"\n")
+years = ["2019-12-01"]
+for i in range(1,13):
+    if len(str(i))==1:
+        a = f"2020-0{i}-01"
+    else:
+        a = f"2020-{i}-01"
+    years.append(a)
+print(years)
+
+
+
+for i in tqdm(list(set(crimes))):
+    for i_ in list(set(cities)):
+        for index,year in enumerate(years):
+            with open(r"J:\PyCharm项目\项目\中国裁判文书网\selenium路线\文书爬取模块\获取城市限定模块\crime_and_cities.txt", "a",encoding="utf-8")as f1:
+                if index+1 <= len(years)-1:
+                    f1.write(i_+","+i+","+year+","+years[index+1]+"\n")
+                else:
+                    f1.write(i_ + "," + i + ","+year + "," + "2021-02-01" + "\n")
 
 
 
--- 城市数量.py
+++ 城市数量.py
@@ -9,6 +9,7 @@
 获取中国所有城市列表
 """
 import sys,os,re,requests
+from tqdm import tqdm
 from lxml import etree
 from crawler_tools.user_agent import user_agent as u
 class Citys():
@@ -22,36 +23,70 @@ class Citys():
         r1 = requests.get(self.url,headers=header)
         r1.encoding = r1.apparent_encoding
         return r1.text
-    def xpath_parse(self):
+    def xpath_parse(self,condition):
         #省
-        lxml = etree.HTML(self.get_url())
-        provinces = lxml.xpath("//tr/td/a/text()")
-        #市
-        citys = lxml.xpath("//tr/td/a/@href")
-        for n,i in enumerate(citys):
-            url = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/'+i
-            print("市：",url)
-            r2 = requests.get(url,headers=u.user_agent())
-            r2.encoding = r2.apparent_encoding
-            countys = etree.HTML(r2.text).xpath("//tr/td[2]/a//text()")
-            #print(xpath)
-            #县
-            countys_href = etree.HTML(r2.text).xpath("//tr/td[2]/a/@href")
-            #print(countys)
-            for ns,county in enumerate(countys_href):
-                url = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/'+county
-                print("县:",url)
-                r3 = requests.get(url, headers=u.user_agent())
-                r3.encoding = r3.apparent_encoding
-                zones = etree.HTML(r3.text).xpath("//tr/td[2]/a//text()")
-                for zone in zones:
-                    yield provinces[n]+","+countys[ns]+","+zone
+        if condition == "provinces":
+            lxml = etree.HTML(self.get_url())
+            provinces = lxml.xpath("//tr/td/a/text()")
+            print("省：", provinces)
+            return provinces
+        elif condition == "city":
+            city = []
+            lxml = etree.HTML(self.get_url())
+            citys = lxml.xpath("//tr/td/a/@href")
+            for n, i in tqdm(enumerate(citys)):
+                url = self.url + i
+                r2 = requests.get(url, headers=u.user_agent())
+                r2.encoding = r2.apparent_encoding
+                countys = etree.HTML(r2.text).xpath("//tr/td[2]/a//text()")
+                for citys in countys:
+                    city.append(citys)
+            return city
+        elif condition=="zone":
+            area = []
+            lxml = etree.HTML(self.get_url())
+            citys = lxml.xpath("//tr/td/a/@href")
+            for n, i in tqdm(enumerate(citys)):
+                url = self.url + i
+                r2 = requests.get(url, headers=u.user_agent())
+                r2.encoding = r2.apparent_encoding
+                countys_href = etree.HTML(r2.text).xpath("//tr/td[2]/a/@href")
+                for ns, county in enumerate(countys_href):
+                    url = self.url+ county
+                    print(url)
+                    r3 = requests.get(url, headers=u.user_agent())
+                    r3.encoding = r3.apparent_encoding
+                    zones = etree.HTML(r3.text).xpath("//tr/td[2]/a//text()")
+                    for zone in zones:
+                        print(zone)
+                        area.append(zone)
+            return area
+        else:
+            all = []
+            lxml = etree.HTML(self.get_url())
+            provinces = lxml.xpath("//tr/td/a/text()")
+            citys = lxml.xpath("//tr/td/a/@href")
+            for n, i in tqdm(enumerate(citys)):
+                url = self.url + i
+                r2 = requests.get(url, headers=u.user_agent())
+                r2.encoding = r2.apparent_encoding
+                citys1 = etree.HTML(r2.text).xpath("//tr/td[2]/a/text()")
+                countys_href = etree.HTML(r2.text).xpath("//tr/td[2]/a/@href")
+                for ns, county in enumerate(countys_href):
+                    url = self.url + county
+                    r3 = requests.get(url, headers=u.user_agent())
+                    r3.encoding = r3.apparent_encoding
+                    zones = etree.HTML(r3.text).xpath("//tr/td[2]/a//text()")
+                    for zone in zones:
+                        all.append(provinces[n]+","+citys1[ns]+","+zone)
+                        print(provinces[n]+","+citys1[ns]+","+zone)
+            return all
+    def download(self, condition):
+        for i in self.xpath_parse(condition=condition):
+            with open(self.file + f"\\{condition}.txt", "a+", encoding="utf-8")as f:
+                f.write(i + "\n")
 
-    def download(self):
-        for i in self.xpath_parse():
-            with open(self.file+"\provinces_city_zone.txt","a+",encoding="utf-8")as f:
-                f.write(i+"\n")
-Citys().download()
+Citys().download(condition="zo")
 def citys():
     with open(r"J:\PyCharm项目\项目\中国裁判文书网\selenium路线\数据清洗模块\城市（副本）.txt", "r", encoding="utf-8")as f:
         i = 1
